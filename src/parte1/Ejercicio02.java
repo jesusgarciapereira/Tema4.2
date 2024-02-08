ceb0767a5fd5727e7762e91fa536b2338ef6cd46
jesusgarciapereira <jesus.garciap@iesnervion.es>
2024-02-08 14:29:21 +0100

    Hecha la versión avanzada

--- src/parte1/Ejercicio02.java
+++ src/parte1/Ejercicio02.java
@@ -62,13 +62,76 @@ public class Ejercicio02 {
 	}
 
 	/**
+	 * Funcion que representa la version avanzada del juego "Acierta la
+	 * contrasennia"
+	 */
+	public static void aciertaLaContrasenniaAvanzado() {
+		// Mensaje de bienvenida
+		System.out.println("Bienvenidos a la versión avanzada del juego \"Acierta la contraseña\"");
+
+		// Le pedimos al jugador 1 la contraseña
+		System.out.println("Jugador 1, introduzca la contraseña");
+		// Y la asignamos a la primera frase
+		primeraFrase = sc.nextLine();
+
+		// Le pedimos al jugador 2 que trate de adivinar la contraseña
+		System.out.println("Jugador 2, adivine la contraseña (teniendo en cuenta mayúsculas y minúsculas)");
+		// Y la asignamos a la segunda frase
+		segundaFrase = sc.nextLine();
+
+		// Mientras ambas frases sean diferentes, repetiremos todas estas instrucciones
+		while (!segundaFrase.equals(primeraFrase)) {
+			// Bucle for que recorre cada caracter de la primera frase
+			for (int i = 0; i < primeraFrase.length(); i++) {
+				// Si coinciden los caracteres de ambas frases
+				if (primeraFrase.charAt(i) == segundaFrase.charAt(i))
+					// Se muestra dicho caracter
+					System.out.print(primeraFrase.charAt(i));
+				// En caso contrario
+				else
+					// Se muestra un asterisco
+					System.out.print("*");
+			}
+			// Salto de línea
+			System.out.println();
+		
+		// Y se le pedirá que lo intente de nuevo
+		System.out.println("Inténtelo de nuevo (recuerde que las minúsculas y las mayúsculas cuentan)");
+		// Y la asignamos nuevamente a la segunda frase
+		segundaFrase = sc.nextLine();
+		}
+		// Al salir del bucle while significará que hemos acertado
+		System.out.println("Enhorabuena, has acertado. La contraseña era " + primeraFrase);
+	}
+
+	/**
 	 * Clase main
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// Llamamos a la función aciertaLaContrasennia() para que muestre todo el desarrollo del juego
+		// Variable con la opción que elegirá el jugador
+		String juguemos = "";
+		
+		// Llamamos a la función aciertaLaContrasennia() para que muestre todo el
+		// desarrollo del juego
 		aciertaLaContrasennia();
+		
+		// Preguntamos al jugador si quiere probar la versión avanzada
+		System.out.println("¿Quieres probar la versión avanzada? (s/n)");
+		// Y asignamos lo que escriba en el teclado
+		juguemos = sc.nextLine();
 
+		// Si ha escrito "s"
+		if(juguemos.equalsIgnoreCase("s"))			
+		// Llamamos a la función aciertaLaContrasenniaAvanzado() para que muestre todo
+		// el desarrollo del juego en la versión avanzada
+		aciertaLaContrasenniaAvanzado();
+		// En caso contrario
+		else
+			// No hay juego
+			System.out.println("Pues nada... adiós");
+		
 		// Cerramos el Scanner
 		sc.close();
 
