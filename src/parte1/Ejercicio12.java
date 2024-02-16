68a86ae2fa47ee8c5a21a5da84265a00b83a3dac
jesusgarciapereira <jesus.garciap@iesnervion.es>
2024-02-16 14:04:23 +0100

    Ejercicio12 Terminado

--- src/parte1/Ejercicio12.java
+++ src/parte1/Ejercicio12.java
@@ -1,14 +1,46 @@
 package parte1;
 
+import java.util.Scanner;
+
 /**
- * Clase que representa al Ejercicio 11 del apartado Tema 4.2 Parte 1: Realizar
- * un programa descodificador. La solución es tan sencilla como utilizar la
- * función diseñada en el ejercicio anterior intercambiando los conjuntos entre
- * sí.
+ * Clase que representa al Ejercicio 12 del apartado Tema 4.2 Parte 1: Leer una
+ * frase y encontrar la palabra de mayor longitud. El programa debe imprimir
+ * tanto la palabra como el número de caracteres de la misma.
  */
 public class Ejercicio12 {
 
 	public static void main(String[] args) {
+		// Frase que le pedimos al usuario
+		String frase = "";
+		// Tabla con cada una de las palabras
+		String[] palabras;
+		// Palabra más larga
+		String palabraMasLarga = "";
+
+		// Activamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Le pedimos al usuario una frase
+		System.out.println("Introduzca una frase");
+		// Y la asignamos
+		frase = sc.nextLine();
+		
+		// Asignamos a palabras cada una de las palabras de la frase llamando al método split
+		palabras = frase.split(" ");
+		
+		// Bucle for que recorrerá cada palabra de la tabla palabras
+		for (int i = 0; i < palabras.length; i++) {
+			// Si la longitud de la palabra es mayor que la de la palabra más larga
+			if (palabras[i].length() > palabraMasLarga.length())
+				// La asignamos como actual palabra más larga
+				palabraMasLarga = palabras[i];
+		}
+		
+		// Mostramos el resultado
+		System.out.println("En la frase \"" + frase + "\" la palabra más larga es: \"" + palabraMasLarga + "\"");
+		
+		// Cerramos el Scanner
+		sc.close();
 
 	}
 
