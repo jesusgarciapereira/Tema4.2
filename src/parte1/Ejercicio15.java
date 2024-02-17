458ff13de9246eedef01ed55d14ef7a412bc852d
Jesús <jesus.garciap@iesnervion.es>
2024-02-17 13:37:21 +0100

    Ejercicio15 Hecho

---
+++ src/parte1/Ejercicio15.java
@@ -0,0 +1,96 @@
+package parte1;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Clase que representa al Ejercicio 14 del apartado Tema 4.2 Parte 1: Juego del
+ * anagrama
+ */
+public class Ejercicio15 {
+
+	/**
+	 * Objeto de tipo Random
+	 */
+	static Random r = new Random();
+
+	/**
+	 * Scanner activado
+	 */
+	static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * Funcion que devuelve el texto del parametro pero desordenado
+	 * 
+	 * @param texto String introducido por el usuario
+	 * @return El texto del parametro pero desordenado
+	 */
+	public static String desordenaTexto(String texto) {
+		// Texto desordenado que devolverá la función
+		String textoDesordenado = "";
+		// Tabla que guardará enteros con las posiciones ya seleccionadas del texto
+		int posicionesYaSeleccionadas[] = new int[texto.length()];
+		// Número generado aleatoriamente
+		int intAleatorio;
+
+		// Relleno la tabla de posiciones ya seleccionadas con -1
+		Arrays.fill(posicionesYaSeleccionadas, -1);
+
+		// Bucle for que ejecutará las instrucciones tantas veces como caracteres haya
+		// en el texto
+		for (int i = 0; i < texto.length(); i++) {
+			do {
+				// Asignamos a intAleatorio un número entre el 0 y la longitud del texto del
+				// parámetro excluido
+				intAleatorio = r.nextInt(0, texto.length());
+				// Si dicho número está presente en las posiciones ya seleccionadas, generará
+				// otro hasta que deje de cumplirse esa condición
+			} while (Arrays.binarySearch(posicionesYaSeleccionadas, intAleatorio) >= 0);
+
+			// Concatenaremos el caracter aleatorio obtenido del texto con nuestro texto
+			// desordenado
+			textoDesordenado += texto.charAt(intAleatorio);
+			// Agregamos dicho número aleatorio a nuestra tabla de posiciones ya
+			// seleccionadas
+			posicionesYaSeleccionadas[0] = intAleatorio;
+			// Ordenamos la tabla de posiciones ya seleccionadas
+			Arrays.sort(posicionesYaSeleccionadas);
+		}
+
+		// Devolverá el texto desordenado
+		return textoDesordenado;
+	}
+
+	/**
+	 * Metodo main
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Texto que le pediremos al jugador 1
+		String texto = "";
+		// Texto desordenado que mostraremos
+		String textoDesordenado = "";
+//		// Texto que le pediremos al jugador 2
+//		String intentaAcertar = "";
+
+		// Mientras el texto esté vacío se ejecutarán estas instrucciones
+		while(texto.isEmpty()) {
+		// Le pedimos al usuario un texto
+		System.out.println("Jugador 1, introduzca un texto");
+		// Y lo asignamos al texto
+		texto = sc.nextLine();
+		}
+		// Asignamos al texto desordenado el valor vuelto por la función
+		// desordenaTexto()
+		textoDesordenado = desordenaTexto(texto);
+
+		// Y lo mostramos
+		System.out.println(textoDesordenado);
+		
+		// Cerramos el Scanner
+		sc.close();
+	}
+
+}
